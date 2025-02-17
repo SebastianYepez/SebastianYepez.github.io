f5438b558114c9410a8b2a2aafc1249267a3d614
Sebastian Yepez <121584176+SebastianYepez@users.noreply.github.com>
2025-02-16 20:59:29 -0800

    Removed v1 folder and modified .gitignore

--- v1/v1script.js
+++
@@ -1,9 +0,0 @@
-/* Function for hamburger menu */
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    //appends a class open to .menu-links, deletes if clicked again
-    icon.classList.toggle("open");
-    //appends a class open to .hamburger-icon, deletes if clicked again
-}
